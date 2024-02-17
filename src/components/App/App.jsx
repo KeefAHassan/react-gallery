e511ae62754bd2072b579db6659541d7e556983c
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-02-17 11:31:16 -0600

    fetch gallery list from the server

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [galleryList, setGalleryList] = useState([]);
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/api/gallery'
+    })
+      .then((response) => {
+        console.log('Entire response:', response);
+        console.log('Just the data:', response.data);
+
+        setGalleryList(response.data);
+      })
+      .catch(function (error) {
+        console.log('Error on get:', error);
+      });
+  }
+
+  useEffect(() => {
+    fetchGallery();
+  }, [])
+
     return (
       <div>
         <header>
