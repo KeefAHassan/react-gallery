3b5a1e0f04e28e5b8ee50dc968674e9bcfd405fa
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-02-18 11:10:51 -0600

    update gallery item

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,14 +4,21 @@ function GalleryItem({ item, increaseLikes }) {
   const [showImage, setShowImage] = useState(true);
   return (
     <div data-testid="galleryItem">
-      {showImage ? (
-        <img src={item.url} alt="" onClick={() => setShowImage(!showImage)} data-testid="galleryItem" />
-      ) : ( 
-        <div onClick={() => setShowImage(!showImage)} data-testid="galleryItem">
-          <p> {item.description} </p>
-        </div>
-      )}
-      <button onClick={() => increaseLikes()} data-testid="like"> love it! </button>
+        <p>{item.title}</p>
+      <div data-testid="toggle" onClick={() => setShowImage(!showImage)}>
+        {showImage ? (
+          <img src={item.url} alt="" />
+        ) : (
+          <div>
+            <p> {item.description} </p>
+          </div>
+        )}
+      </div>
+
+      <button onClick={() => increaseLikes()} data-testid="like">
+        {" "}
+        love it!{" "}
+      </button>
       <p>{item.likes} people like this </p>
     </div>
   );
